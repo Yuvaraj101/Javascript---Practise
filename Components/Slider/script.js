6fe16baba556b93ca61a307844e50bf8bca45212
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-09-14 20:46:16 +0530

    added slider functionality

--- Components/Slider/script.js
+++ Components/Slider/script.js
@@ -4,3 +4,30 @@ var slider = document.querySelector(".slider-container")
 var left = document.getElementById("btn1")
 var right = document.getElementById("btn2")
 
+var margin = 0
+
+left.addEventListener("click",function()
+{
+    margin = margin + 100
+    if(margin > 200)
+    {
+        margin =0
+    }
+    slider.style.marginLeft = "-"+margin+"vw"
+})
+
+right.addEventListener("click",function()
+{
+    if(margin < 0)
+    {
+        margin = 200
+        slider.style.marginLeft = "-"+margin+"vw"
+        
+    }
+    else
+    {
+        margin = margin - 100
+        slider.style.marginLeft = "-" + margin + "vw"
+
+    }
+})
