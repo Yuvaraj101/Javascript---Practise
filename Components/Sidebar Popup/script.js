fc7b1c927262536185336d15d96d44fca0cd8e29
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-09-16 12:56:22 +0530

    updated changes

---
+++ Components/Sidebar Popup/script.js
@@ -0,0 +1,26 @@
+
+var breadbtn = document.getElementById("bread-button")
+
+var sidebar = document.querySelector(".sidebar")
+
+var count = 0
+
+breadbtn.addEventListener("click",function()
+{
+    
+    
+    count = count + 1
+    if(count==1)
+    {
+
+        sidebar.style.marginLeft = "0vw"
+    }
+    else if(count==2)
+    {
+        
+        sidebar.style.marginLeft = "-100vw"
+        count = 0
+    }
+    console.log(count);
+
+})
