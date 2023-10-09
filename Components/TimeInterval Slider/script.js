34504208d2c1e57622aa1c0a6cfb4be519e9f272
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-10-09 23:01:42 +0530

    .

---
+++ Components/TimeInterval Slider/script.js
@@ -0,0 +1,56 @@
+leftbtn = document.getElementById("leftbtn")
+rightbtn = document.getElementById("rightbtn")
+
+slider = document.querySelector(".slider-container")
+
+var margin = 0
+leftbtn.addEventListener("click",function()
+{
+    if(margin<200)
+    {
+        margin = margin + 100
+
+        slider.style.marginLeft = "-" + margin + "vw"
+    }
+    else
+    {
+        margin = 0
+        slider.style.marginLeft = "-" + margin + "vw"
+    }
+})
+
+rightbtn = document.getElementById("rightbtn")
+
+rightbtn.addEventListener("click",function()
+{
+   
+    if(margin<0)
+    {
+        margin = 200
+        slider.style.marginLeft = "-" + margin + "vw"
+    }
+    else
+    {
+        margin = margin - 100
+       
+        slider.style.marginLeft = "-" + margin + "vw"
+
+    }
+
+})
+
+//to make the slider move automactically after 4 seconds of delay
+
+// setInterval(() => {
+//     if(margin<200)
+//     {
+//         margin = margin + 100
+
+//         slider.style.marginLeft = "-" + margin + "vw"
+//     }
+//     else
+//     {
+//         margin = 0
+//         slider.style.marginLeft = "-" + margin + "vw"
+//     }
+// }, 4000);
