beaf2bfa5e1c761191bd1079894608ebd39ee9e6
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-10-13 22:22:26 +0530

    responsive image gallery

--- Components/Display on Scroll/script.js
+++ Components/Display on Scroll/script.js
@@ -1,7 +1,20 @@
-img = document.querySelector(".box")
+img = document.querySelectorAll(".box")
+
+console.log(img);
 
 window.addEventListener("scroll",function(){
-    
+
     height = window.innerHeight
-    console.log(height);
+
+    img.forEach(function(el)
+    {
+        console.log(el);
+        var boxy = el.getBoundingClientRect().top
+        if (boxy < height){
+            el.style.opacity = 1
+        }
+    })
+    
+    // var boxy = img.getBoundingClientRect().top
+
 })
