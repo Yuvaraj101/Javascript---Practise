68830110b8b0fcc29eb49e9a408a6279dd27eb21
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-11-23 23:31:36 +0530

    .

--- Map, Filter/Filter.js
+++ Map, Filter/Filter.js
@@ -1,3 +1,15 @@
+// note :
+// Filter method returns an new array which 
+// satisfies the provided function within 
+// it as a parameter 
+
 const arr = [10,20,30,40,50,60]
 
+const res = arr.filter(ageChk)
+
+function ageChk(e){
+    return e >= 20;
+}
+
 console.log(arr);
+console.log(res);
