a48d910a28ca2bbf95a20b73cc6dba03e77f0c73
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-11-27 22:27:04 +0530

    .

--- ES6/Spread.js
+++ ES6/Spread.js
@@ -1,8 +1,28 @@
 
-const arr = ['hi','hello','how','are','you']
+// const arr = ['hi','hello','how','are','you']
 
-console.log(arr);
+// console.log(arr);
 
-const res = [...arr,'1','2']
+// const res = [...arr,'1','2']
+
+// console.log(res);
+
+// ---------------------
+// objects 
+
+const cars = {
+    brand : 'bmw',
+    model : 'm5 comp',
+    color : 'green yellow'
+
+}
+
+const cars2 = {
+    prod : 'speed',
+    type : 'auto',
+    color : 'white'
+}
+
+const res = {...cars , ...cars2}
 
 console.log(res);
